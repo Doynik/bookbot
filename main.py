cbe54dd10e939e0fa53b26bf1674feee79d3dd68
Erynik <rynkir@gmail.com>
2026-04-21 15:20:44 +0100

    removed hardcoded book path

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
-from stats import get_num_words, get_num_chars
+import sys
+from stats import (
+    get_num_words, 
+    get_num_chars, 
+    get_sorted_chars
+)
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    num_words = get_num_words(book_text)
-    print(f"{num_words} words found in document")
-    num_chars = get_num_chars(book_text)
-    print(num_chars)
+    if len(sys.argv) == 2:
+        book_path = sys.argv[1]
+        book_text = get_book_text(book_path)
+        num_words = get_num_words(book_text)
+        num_chars = get_num_chars(book_text)
+        sorted_chars = get_sorted_chars(num_chars)
+        print_report(book_path, num_words, sorted_chars)
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
 
 def get_book_text(book_path):
     with open(book_path) as f:
         return f.read()
 
-main()
+def print_report(book_path,num_words,sorted_chars):
+    print("======== BOOKBOT REPORT ========")
+    print(f"Analyzing book found at {book_path}")
+    print("-------- Word Count --------")
+    print(f"Found {num_words} total words")
+    print("-------- Character Count --------")
+    for item in sorted_chars:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item["char"]}: {item["num"]}")
+    print("============ END ============")
 
+main()
--- stats.py
+++ stats.py
@@ -12,3 +12,19 @@ def get_num_chars(book_text):
         else:
             char_count[char] = 1
     return char_count
+
+def sort_on(d):
+    return d["num"]
+
+def get_sorted_chars(num_chars_dict):
+    sorted = []
+    for char,count in num_chars_dict.items():
+        sorted.append({"char": char, "num": count})
+    sorted.sort(reverse=True,key=sort_on)
+    return sorted
+
+        
+    
+
+
+
