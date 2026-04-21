94c129e15f2369860fb7aacd0f1039cd6ab79997
Erynik <rynkir@gmail.com>
2026-04-21 13:47:53 +0100

    Reworked main + stats functions for simplicity

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from stats import get_num_words
-
-def get_book_text(book_path):
-    with open(book_path) as f:
-        file_contents = f.read()
-    return file_contents
+from stats import get_num_words, get_num_chars
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(book_text)
     print(f"{num_words} words found in document")
+    num_chars = get_num_chars(book_text)
+    print(num_chars)
+
+def get_book_text(book_path):
+    with open(book_path) as f:
+        return f.read()
 
 main()
 
--- stats.py
+++ stats.py
@@ -1,6 +1,14 @@
 def get_num_words(book_text):
-    num_words = 0 
     words = book_text.split()
-    for word in words:
-        num_words += 1
-    print(f"{num_words} words found in the document")
+    return len(words)
+
+def get_num_chars(book_text):
+    char_count = {}
+
+    for char in book_text:
+        char = char.lower()
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
